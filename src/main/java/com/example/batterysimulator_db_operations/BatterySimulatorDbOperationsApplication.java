95aa9d563feb2f9fbd745258a39f4fdb6ab28709
Mark Veerasingam <markveer70@gmail.com>
2023-12-28 22:55:47 +0000

    First Commit

---
+++ src/main/java/com/example/batterysimulator_db_operations/BatterySimulatorDbOperationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.batterysimulator_db_operations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatterySimulatorDbOperationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatterySimulatorDbOperationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/batterysimulator_db_operations/BatterySimulatorDbOperationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.batterysimulator_db_operations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatterySimulatorDbOperationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
