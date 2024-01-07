cd72f7c8f232af94502317c6e6ebc39f147e62b8
Lucas Jeanes <lucasjeanes@gmail.com>
2024-01-07 20:27:25 +0000

    Finalized connection to JobManager. DbOperations successfully reads incoming sim results and saves them to H2 Database.

--- src/main/java/com/example/batterysimulator_db_operations/POJOs/Simulation.java
+++ src/main/java/com/example/batterysimulator_db_operations/POJOs/Simulation.java
@@ -1,5 +1,6 @@
 package com.example.batterysimulator_db_operations.POJOs;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -17,21 +18,14 @@ public class Simulation {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long simId;
+
     @Enumerated(EnumType.STRING)
     private SimulationType simulationType;
+
     @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "simulation_simId")
     @JsonManagedReference
     private List<SimulationResults> simulationResults;
-
     @Column(nullable = true)
-    private double time;
-    @Column(nullable = true)
-    private double upperVoltage;
-    @Column(nullable = true)
-    private double lowerVoltage;
-    @Column(nullable = true)
-    private double nominalCell;
-    @Column(nullable = true)
-    private double controlCurrent;
+    private String jobStarted;
 }
